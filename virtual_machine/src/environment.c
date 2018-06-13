4eec56ab0035338171ae078da3f3bc2bdf5f76ab
Brendan <brendan_1391@yahoo.com>
2018-06-13 13:59:17 -0700

    Cycle Handling

---
+++ virtual_machine/src/environment.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environment.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpierce <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/13 13:09:44 by bpierce           #+#    #+#             */
+/*   Updated: 2018/06/13 13:09:54 by bpierce          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "corewar.h"
+
+void		init_environment(t_corewar *core)
+{
+	core->env.cycle = 0;
+	core->env.max_cycles = get_max_cycles(CYCLE_TO_DIE);
+	core->env.game_speed = GAME_SPEED;
+	core->env.cycle_to_die = CYCLE_TO_DIE;
+	core->env.cycle_delta = CYCLE_DELTA;
+	core->env.nbr_live = NBR_LIVE;
+	core->env.max_checks = MAX_CHECKS;
+}
+
+uint64_t		get_max_cycles(uint64_t init)
+{
+	if (init <= CYCLE_DELTA)
+		return (0);
+	return (init + get_max_cycles(init - CYCLE_DELTA));
+}
