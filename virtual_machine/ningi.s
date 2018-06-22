9e1c8f03990e380512691668263f6e9ab2f07733
Brendan <brendan_1391@yahoo.com>
2018-06-21 22:00:18 -0700

    updated ningi

--- virtual_machine/ningi.s
+++ virtual_machine/ningi.s
@@ -14,3 +14,42 @@ label2:	fork %3
 		live %0
 		add r1, r2, r2
 		fork %:label2
+label3:	fork %3
+		sti r1, %4, %4
+		live %0
+		add r1, r2, r2
+		fork %:label3
+		sti r1, %200, %200
+		sti r1, %100, %100
+		sti r1, %50, %50
+label4:	fork %3
+		sti r1, %4, %4
+		live %0
+		add r1, r2, r2
+		fork %:label4
+label5:	fork %3
+		sti r1, %4, %4
+		live %0
+		add r1, r2, r2
+		fork %:label5
+		sti r1, %200, %200
+		sti r1, %100, %100
+		sti r1, %50, %50
+label6:	fork %3
+		sti r1, %4, %4
+		live %0
+		add r1, r2, r2
+		fork %:label6
+label7:	fork %3
+		sti r1, %4, %4
+		live %0
+		add r1, r2, r2
+		fork %:label7
+		sti r1, %200, %200
+		sti r1, %100, %100
+		sti r1, %50, %50
+label8:	fork %3
+		sti r1, %4, %4
+		live %0
+		add r1, r2, r2
+		fork %:label8
